1a605f28b65618298812014f36018a86dd827024
theweawithoutname <lucciano_c9@hotmail.com>
2025-10-15 20:23:58 -0300

    a

--- src/components/SummonerIcon.tsx
+++ src/components/SummonerIcon.tsx
@@ -1,13 +1,17 @@
 // SummonerIcon.tsx
+/* 
+  *COMPONENTE vISUAL DEL ICONO
+  
+*/
 
 import React from 'react';
 import {type SummonerIconProps } from '../types/Props';
 
 
-const SummonerIcon: React.FC<SummonerIconProps> = ({ iconId, altText }) => {
+const SummonerIcon: React.FC<SummonerIconProps> = ({ iconUrl, altText }) => {
   return (
     <img 
-      src={iconId} 
+      src={iconUrl} 
       alt={altText}
       className="w-20 h-20 rounded-full border-4 border-yellow-500 shadow-lg object-cover"
     />
